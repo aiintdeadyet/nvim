ebe7c8874f1fcbb74ee4f9a3010e3542ce6a1ba0
James Critchlow <jamesdcritchlow@gmail.com>
2024-10-21 15:42:55 -0600

    updated custom config for python

--- lua/plugins/dap-python.lua
+++ lua/plugins/dap-python.lua
@@ -8,11 +8,20 @@ return { -- python dap addapter
 	config = function(_, opts)
 		-- local path = "~/.config/nvim/.virtualenvs/debugpy/bin/python3" -- if you want to setup a python compiler as part of the nvim config
 		require("dap-python").setup("python3")
+		-- example custom config
+		-- table.insert(require('dap').configurations.python, {
+		-- 	name = 'My custom launch configuration',
+		-- 	type = 'debugpy',
+		-- 	request = 'launch',
+		-- 	program = '${file}',
+		-- 	console = 'integratedTerminal', -- this line might cause problems
+		-- 	-- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+		-- })
 		table.insert(require('dap').configurations.python, {
-			name = 'My custom launch configuration',
+			name = "autograder",
 			type = 'debugpy',
 			request = 'launch',
-			program = '${file}',
+			program = 'autograder.py',
 			console = 'integratedTerminal', -- this line might cause problems
 			-- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
 		})
