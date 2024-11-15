6362258eba158ceef2d3640e477a82159ac828cf
James Critchlow <jamesdcritchlow@gmail.com>
2024-11-15 12:33:32 -0700

    No longer adds headers in c or cpp

--- lua/settings/pyright-settings.lua
+++ lua/settings/pyright-settings.lua
@@ -10,20 +10,38 @@ lspconfig.pyright.setup {
 				diagnosticMode = "workspace", -- openFilesOnly, workspace
 				typeCheckingMode = "basic", -- off, basic, strict
 				useLibraryCodeForTypes = true,
-				
+
 			}
 		}
 	}
 }
 
+lspconfig.clangd.setup {
+	capabilities = cmp,
+       cmd = {
+           "clangd",
+           "--background-index",
+           "-j=12",
+           "--query-driver=/usr/bin/**/clang-*,/bin/clang,/bin/clang++,/usr/bin/gcc,/usr/bin/g++",
+           "--clang-tidy",
+           "--clang-tidy-checks=*",
+           "--all-scopes-completion",
+           "--cross-file-rename",
+           "--completion-style=detailed",
+           "--header-insertion-decorators",
+           "--header-insertion=never",
+           "--pch-storage=memory",
+       }
+}
+
 -- lspconfig.pylsp.plugins.rope_autoimport.completions.enabled = false	
-lspconfig.pylsp.setup{
-	settings = {
-		pylsp = {
-			plugins = {
-				rope_autoimport = false
-			}
-		}
-	}
-}	
+-- lspconfig.pylsp.setup{
+-- 	settings = {
+-- 		pylsp = {
+-- 			plugins = {
+-- 				rope_autoimport = true
+-- 			}
+-- 		}
+-- 	}
+-- }	
 
