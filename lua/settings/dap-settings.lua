a46b037bcdae22dbbdc1ac9f4d83669d56dd5851
aiin'tdeadyet <jamesdcritchlow@gmail.com>
2025-01-07 19:01:50 -0700

    Updated my a C\C++ dap configurations

--- lua/settings/dap-settings.lua
+++ lua/settings/dap-settings.lua
@@ -76,3 +76,19 @@ dap.configurations.cpp = {
 		runInTerminal = true,
 	}
 }
+
+dap.configurations.c = {
+	{
+		name = "Custom",
+		type = "codelldb",
+		request = "launch",
+		program = function ()
+			vim.cmd(':silent exec "!make debug"') -- silently run make
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+		runInTerminal = true,
+	}
+}
